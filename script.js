4855d8401bc1f8189b88f7558b072c5105f3142e
Josh K <47252616+githubfan@users.noreply.github.com>
2024-01-17 19:24:35 +0000

    refactor: fix initialization in variable declarations against recommended approach

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const prompt = require("prompt-sync")();
 
 // Defining latitude and longitude variables
-let latitude;
-let longitude;
+let latitude = 0;
+let longitude = 0;
 
 // Check if latitude is between -90 and 90 and is a number, and if not, make the user enter a valid latitude
 while (true) {
